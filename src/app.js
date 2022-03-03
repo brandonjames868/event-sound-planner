5a71c8edb992836c8ba64f4c9c707ee1cd81bb05
brandonjames868 <brandon.james1@my.uwi.edu>
2022-03-03 17:13:42 -0400

    authentication added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import ProtectedRoute from './auth/protected-route';
 
 import { NavBar, Footer, Loading } from './components';
 import { Home, Profile, ExternalApi } from './views';
@@ -20,8 +21,8 @@ const App = () => {
       <div className="container flex-grow-1">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/external-api" component={ExternalApi} />
+          <ProtectedRoute path="/profile" component={Profile} />
+          <ProtectedRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </div>
       <Footer />
--- src/components/loading.js
+++ src/components/loading.js
@@ -3,7 +3,7 @@ const loadingImg =
   "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
 
 const Loading = () => (
-  <div className="spinner">
+  <div className="spinner h-50 my-auto">
     <img src={loadingImg} alt="Loading..." />
   </div>
 );
